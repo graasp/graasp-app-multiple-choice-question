d02c0eecc21e0ce9bb54a3650ee41dfef37e31d2
Jérémy La Scala <jeremy.lascala@epfl.ch>
2024-01-13 15:35:08 +0100

    test: implement basic test for multiple answers

--- cypress/e2e/player/main.cy.ts
+++ cypress/e2e/player/main.cy.ts
@@ -4,10 +4,15 @@ import {
   MCQ_QUESTION_CY,
   buildDataCy,
   makeMcqAnswersCy,
+  makeMcqMultipleAnswersCy,
 } from '../../../src/config/selectors';
-import { ANSWERS_SETTING, QUESTION_SETTING } from '../../fixtures/appSettings';
+import {
+  ANSWERS_SETTING,
+  ANSWERS_SETTING_MULTI_ANSWERS,
+  QUESTION_SETTING,
+} from '../../fixtures/appSettings';
 
-describe('Player View configured', () => {
+describe('Player View configured for single answer', () => {
   beforeEach(() => {
     cy.setUpApi(
       {
@@ -26,6 +31,33 @@ describe('Player View configured', () => {
     answers.forEach((ans, index) => {
       cy.get(buildDataCy(makeMcqAnswersCy(index))).should('be.visible');
     });
+    cy.get(buildDataCy(makeMcqMultipleAnswersCy(0))).should('not.exist');
+
+    cy.get(buildDataCy(MCQ_QUESTION_CY)).should('be.visible');
+  });
+});
+
+describe('Player View configured for multiple answer', () => {
+  beforeEach(() => {
+    cy.setUpApi(
+      {
+        appSettings: [QUESTION_SETTING, ANSWERS_SETTING_MULTI_ANSWERS],
+      },
+      {
+        context: Context.Player,
+        permission: PermissionLevel.Read,
+      },
+    );
+    cy.visit(`/`);
+  });
+
+  it('test question and answers are visible', () => {
+    const { answers } = ANSWERS_SETTING_MULTI_ANSWERS.data;
+    answers.forEach((ans, index) => {
+      cy.get(buildDataCy(makeMcqMultipleAnswersCy(index))).should('be.visible');
+    });
+
+    cy.get(buildDataCy(makeMcqAnswersCy(0))).should('not.exist');
 
     cy.get(buildDataCy(MCQ_QUESTION_CY)).should('be.visible');
   });
